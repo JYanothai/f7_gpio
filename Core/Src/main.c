a345f6660c8ad87eebe18b08423bc08e41be071c
Jate Kab <61010260@kmitl.ac.th>
2022-05-02 09:30:17 +0700

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -101,9 +101,12 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  
+	  
 	  auto state = HAL_GPIO_ReadPin(PI14_IN_GPIO_Port, PI14_IN_Pin) == GPIO_PIN_SET;
 	  if(state == 1){
 		  setter = 1;
+		  
 		  continue;
 	  }
 	  if(state == 0 && setter == 1){
